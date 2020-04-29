176d1b6f343ebadebfabbf88b6ac131a0c8b3b76
edsondepaulla <edsondepaulla@gmail.com>
2020-04-29 15:44:37 -0300

    test

--- www/js/index.js
+++ www/js/index.js
@@ -25,13 +25,13 @@ var Location = {
                 break;
             case cordova.plugins.diagnostic.permissionStatus.DENIED:
                 if (cordova.platformId === "android") {
-                    Location.onError("User denied permission to use location");
-                    $('#request-authorization').removeAttr('disabled');
+                    Location.openSettings();
+                    Location.checkState();
                 }
                 break;
             case cordova.plugins.diagnostic.permissionStatus.DENIED_ALWAYS:
-                // Android only
-                Location.onError("User denied permission to use location");
+                Location.openSettings();
+                Location.checkState();
                 break;
         }
     },
@@ -40,7 +40,6 @@ var Location = {
         cordova.plugins.diagnostic.requestLocationAuthorization(
             Location.handleLocationAuthorizationStatus,
             function (error) {
-                alert("1 An error occurred: " + error);
                 Location.checkState();
             }
         );
@@ -50,101 +49,79 @@ var Location = {
         cordova.plugins.locationAccuracy.canRequest(function (canRequest) {
             if (canRequest) {
                 cordova.plugins.locationAccuracy.request(function () {
-                        Location.handleSuccess("Location accuracy request successful");
+                        Location.checkState();
                     }, function (error) {
-                        Location.onError("Error requesting location accuracy: " + JSON.stringify(error));
                         if (error) {
-                            // Android only
-                            Location.onError("error code=" + error.code + "; error message=" + error.message);
                             if (cordova.platformId === "android" && error.code !== cordova.plugins.locationAccuracy.ERROR_USER_DISAGREED) {
-                                if (window.confirm("Failed to automatically set Location Mode to 'High Accuracy'. Would you like to switch to the Location Settings page and do this manually?")) {
+                                if (window.confirm("Falha ao definir o Modo de Localização automaticamente como 'Alta Precisão'. Deseja mudar as Configurações de local e fazer isso manualmente?"))
                                     cordova.plugins.diagnostic.switchToLocationSettings();
-                                }
                             }
                         }
-                    }, cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY // iOS will ignore this
+                    }, cordova.plugins.locationAccuracy.REQUEST_PRIORITY_HIGH_ACCURACY
                 );
-            } else {
-                // On iOS, this will occur if Location Services is currently on OR a request is currently in progress.
-                // On Android, this will occur if the app doesn't have authorization to use location.
-                Location.onError("Cannot request location accuracy");
-            }
+            } else
+                alert("Não é possível solicitar a precisão da localização, favor verificar!");
         });
     },
-
     checkState: function () {
         var canRequest;
         cordova.plugins.locationAccuracy.canRequest(function (_canRequest) {
             canRequest = _canRequest;
             if (canRequest) {
+                Location.requestLocationAccuracy();
                 $('#request-accuracy').removeAttr('disabled');
             } else {
                 $('#request-accuracy').attr('disabled', 'disabled');
+
+                cordova.plugins.diagnostic.isLocationAvailable(
+                    function (available) {
+                        $('#location-available').text(available ? "AVAILABLE" : "UNAVAILABLE");
+                    }, function (error) {
+                        Location.checkState();
+                    }
+                );
+
+                cordova.plugins.diagnostic.isLocationEnabled(
+                    function (enabled) {
+                        $('#location-enabled').text(enabled ? "ENABLED" : "DISABLED");
+                        if (enabled) {
+                            cordova.plugins.diagnostic.isLocationAuthorized(function (authorized) {
+                                $('#location-authorized').text(authorized ? "AUTHORIZED" : "UNAUTHORIZED");
+                            }, function (error) {
+                                Location.checkState();
+                            });
+                        } else {
+                            $('#location-authorized').text("UNKNOWN");
+                        }
+                    },
+                    function (error) {
+                        Location.checkState();
+                    }
+                );
+
+                cordova.plugins.diagnostic.getLocationAuthorizationStatus(
+                    Location.handleLocationAuthorizationStatus,
+                    function (error) {
+                        Location.checkState();
+                    }
+                );
+
+                if (cordova.platformId === "android") {
+                    cordova.plugins.diagnostic.getLocationMode(
+                        function (mode) {
+                            $('#location-mode').text(mode.toUpperCase());
+                            if (canRequest && mode !== "high_accuracy") {
+                                $('#request-accuracy').removeAttr('disabled');
+                            } else {
+                                $('#request-accuracy').attr('disabled', 'disabled');
+                            }
+                        }, function (error) {
+                            Location.checkState();
+                        }
+                    );
+                }
             }
         });
-
-        cordova.plugins.diagnostic.isLocationAvailable(
-            function (available) {
-                $('#location-available').text(available ? "AVAILABLE" : "UNAVAILABLE");
-            }, function (error) {
-                alert("2 An error occurred: " + error);
-                Location.checkState();
-            }
-        );
-
-
-        cordova.plugins.diagnostic.isLocationEnabled(
-            function (enabled) {
-                $('#location-enabled').text(enabled ? "ENABLED" : "DISABLED");
-                if (enabled) {
-                    cordova.plugins.diagnostic.isLocationAuthorized(function (authorized) {
-
-
-                        $('#location-authorized').text(authorized ? "AUTHORIZED" : "UNAUTHORIZED");
-                    }, function (error) {
-                        alert("3 An error occurred: " + error);
-                        Location.checkState();
-                    });
-                } else {
-                    $('#location-authorized').text("UNKNOWN");
-                }
-            },
-            function (error) {
-                alert("4 An error occurred: " + error);
-                Location.checkState();
-            }
-        );
-
-        cordova.plugins.diagnostic.getLocationAuthorizationStatus(
-            Location.handleLocationAuthorizationStatus,
-            function (error) {
-                alert("5 An error occurred: " + error);
-                Location.checkState();
-            }
-        );
-
-        if (cordova.platformId === "android") {
-            cordova.plugins.diagnostic.getLocationMode(
-                function (mode) {
-                    $('#location-mode').text(mode.toUpperCase());
-                    if (canRequest && mode !== "high_accuracy") {
-                        $('#request-accuracy').removeAttr('disabled');
-                    } else {
-                        $('#request-accuracy').attr('disabled', 'disabled');
-                    }
-                }, function (error) {
-                    alert("6 An error occurred: " + error);
-                    Location.checkState();
-                }
-            );
-        }
-    },
-    onError: function (error) {
-        alert("7 An error occurred: " + error);
-    },
-    handleSuccess: function (msg) {
-        alert(msg);
-        Location.checkState();
     }
 };
 
